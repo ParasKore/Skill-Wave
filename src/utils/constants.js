729c19f31056be51c0f441b982f7f7db9ba165d5
ParasKore <paraskore144@gmail.com>
2024-05-25 17:57:36 +0530

    first commit

---
+++ src/utils/constants.js
@@ -0,0 +1,58 @@
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import HomeIcon from '@mui/icons-material/Home';
+import CodeIcon from '@mui/icons-material/Code';
+import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+import LiveTvIcon from '@mui/icons-material/LiveTv';
+import SchoolIcon from '@mui/icons-material/School';
+import FaceRetouchingNaturalIcon from '@mui/icons-material/FaceRetouchingNatural';
+import CheckroomIcon from '@mui/icons-material/Checkroom';
+import GraphicEqIcon from '@mui/icons-material/GraphicEq';
+import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
+import PsychologyIcon from '@mui/icons-material/Psychology';
+import CurrencyExchangeIcon from '@mui/icons-material/CurrencyExchange';
+import AddIcon from '@mui/icons-material/Add';
+import ScienceIcon from '@mui/icons-material/Science';
+import PublicIcon from '@mui/icons-material/Public';
+import BiotechIcon from '@mui/icons-material/Biotech';
+import Diversity3Icon from '@mui/icons-material/Diversity3';
+import PrecisionManufacturingIcon from '@mui/icons-material/PrecisionManufacturing';
+import StorageIcon from '@mui/icons-material/Storage';
+import DevicesIcon from '@mui/icons-material/Devices';
+import NetworkPingIcon from '@mui/icons-material/NetworkPing';
+import SdStorageIcon from '@mui/icons-material/SdStorage';
+import AutoStoriesIcon from '@mui/icons-material/AutoStories';
+import InstallDesktopIcon from '@mui/icons-material/InstallDesktop';
+import InvertColorsIcon from '@mui/icons-material/InvertColors';
+
+export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
+
+export const categories = [
+  { name: 'Study', icon: <AutoStoriesIcon />, },
+  { name: 'Coding', icon: <CodeIcon />, },
+  { name: 'Maths', icon: <AddIcon />, },
+  { name: 'Science', icon: <ScienceIcon />, },
+  { name: 'Physics', icon: <PublicIcon />, },
+  { name: 'Chemistry', icon: <InvertColorsIcon /> },
+  { name: 'Biology', icon: <BiotechIcon />, },
+  { name: 'Social Science', icon: <Diversity3Icon />, },
+  { name: 'Psycology', icon: <PsychologyIcon />, },
+  { name: 'Economics', icon: <CurrencyExchangeIcon />, },
+  { name: 'Quantum Mechanics', icon: <PrecisionManufacturingIcon />, },
+  { name: 'React js', icon: <DevicesIcon />, },
+  { name: 'Next js', icon: <CheckroomIcon />, },
+  { name: 'Mongo DB', icon: <StorageIcon />, },
+  { name: 'Express js', icon: <NetworkPingIcon />, },
+  { name: 'Node js', icon: <InstallDesktopIcon />, },
+  { name: 'SQL', icon: <SdStorageIcon />, },
+  
+];
+
+export const demoThumbnailUrl = 'https://i.ibb.co/G2L2Gwp/API-Course.png';
+export const demoChannelUrl = '/channel/UCmXmlB4-HJytD7wek0Uo97A';
+export const demoVideoUrl = '/video/GDa8kZLNhJ4';
+export const demoChannelTitle = 'JavaScript Mastery';
+export const demoVideoTitle = 'Build and Deploy 5 JavaScript & React API Projects in 10 Hours - Full Course | RapidAPI';
+export const demoProfilePicture = 'http://dergipark.org.tr/assets/app/images/buddy_sample.png'
